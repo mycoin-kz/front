ea1ce3b24254cbc0b183f6027cdc180dbee1e4ad
zhassbala <besimbekovr@gmail.com>
2022-11-17 05:02:46 +0600

    updated server config

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 const fs = require('fs')
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    https: {
-      key: fs.readFileSync('./example.com+5-key.pem'),
-      cert: fs.readFileSync('./example.com+5.pem'),
-    },
-  }
+  // devServer: {
+  //   https: {
+  //     key: fs.readFileSync('./example.com+5-key.pem'),
+  //     cert: fs.readFileSync('./example.com+5.pem'),
+  //   },
+  // }
 })
