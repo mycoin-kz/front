678bf60fb01d523bd79ca9fa4d460ebfa6510982
zhassbala <besimbekovr@gmail.com>
2022-11-19 23:02:04 +0600

    resolved problem with watchlist button, facebook login complete

--- src/store/auth/api.js
+++ src/store/auth/api.js
@@ -1,7 +1,9 @@
-export const base_url = 'http://127.0.0.1:8000/api/'
+export const base_url = 'https://127.0.0.1:8000/api/'
 // export const base_url = 'https://back-app-2y9c7.ondigitalocean.app/api/'
 
 export const API_REGISTER = base_url + 'auth/register'
 export const API_LOGIN = base_url + 'auth/login/'
 export const API_LOGOUT = base_url + 'auth/logout/'
+
+export const API_FACEBOOK_LOGIN = base_url + 'auth/facebook/'
 
